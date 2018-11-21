9c07d14d6909d96150869c9299fdb3a81dc9986f
Francisco Molina <fmolina@osi.com.sv>
2018-11-20 22:33:27 -0600

    gallery, js, okta

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,29 +1,3 @@
-/*
-// activar carousel
-$("#myCarousel").carousel();
-
-// indicadores del carousel
-$(".item").click(function(){
-    $("#myCarousel").carousel(1);
-});
-
-// controles del carousel (flechas)
-$(".carousel-control-prev").click(function(){
-    $("#myCarousel").carousel("prev");
-});
-
-$(".carousel-control-next").click(function(){
-    $("#myCarousel").carousel("next");
-});
-
-//scrollReveal
-
-ScrollReveal().reveal('.heading1',{delay:500});
-ScrollReveal().reveal('.p1',{delay:800});
-ScrollReveal().reveal('.box1',{delay:500});
-ScrollReveal().reveal('.box2',{delay:1000});
-ScrollReveal().reveal('.box3',{delay:1500});
-*/
 function slider1() {
     var x = document.getElementById("bio");
     var y = document.getElementById("gallery");
