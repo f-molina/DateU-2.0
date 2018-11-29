be53f36eff55c87f499efc3d9251e7d434e37586
EdwinLovo <00120617@uca.edu.sv>
2018-11-28 18:00:20 -0600

    mostrando usuarios

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const {getOne} = require('../controllers/userController');
+const userSchema = require('../models/users');
+
 // Display the dashboard page
 router.get("/", async (req, res) => {
   const user = await getOne(req.user.profile.email);
-  console.log(user);
-  res.render("dashboard",{
-    pics: "asdasd"
+  
+  userSchema.find({}, function(err, usuarios){
+    if(err){console.log(err);}
+    res.render("dashboard", {users: usuarios});
   });
+
 });
 
 
